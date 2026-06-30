ef9fade813f3f3e671bb9af2ebd77727373a8b83
emma <153800714+huangid@users.noreply.github.com>
2026-06-30 14:11:48 -0500

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n = len(coins)
+
+        @lru_cache
+        def f(i, c):
+            if i < 0:
+                return 0 if c == 0 else 1e9
+            if coins[i] > c:
+                return f(i-1, c)
+            return min(f(i-1, c), f(i, c-coins[i])+1)
+        num = f(n-1, amount)
+        return -1 if num is 1e9 else num
